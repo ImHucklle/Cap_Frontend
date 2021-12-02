ef2438ba7f705e7ab549b014e023fde9dabfa319
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-12-02 15:09:41 -0500

    Stripped the generic react files to what the project needs. added axios and bootstrap.

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state ={};
+    }
+
+    render(){
+        return (
+            <h1>App.jsx Component/What's to Eat?</h1>
+        )
+    }
+}
+
+export default App;
