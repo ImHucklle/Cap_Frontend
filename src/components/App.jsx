c67b54c7f77edada592600eea5c5e9d0add5dc85
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-12-06 15:21:23 -0500

    Fixed error in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
-import { HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import { spoonacularAPIKey } from '../keys';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
 import Login from './Login/Login';
 import Register from './Register/Register';
 import Landing  from './Landing/Landing';
 
-
 function App() {
-    
-    // const [user, setUser] = useState({})
-    // const [userLogin, setUserLogin] = useState([])
-    // const [jwt, setJwt] = useState()
-    // const [loadData, setLoadData] = useState(false)
-
-    // //Getting token from local storage
-    // const getUserJWT = () => {
-    //     const jwt = localStorage.getItem('token');
-    //     try {
-    //       const user = jwtDecode(jwt);
-    //       setUser(user)
-    //       console.log("get user jwt call", user)
-    //     } catch {
-    //     }
-    //   }
-    
-    //   useEffect(() =>{
-    //     getUserJWT();
-    //     getUserLogin();
-    //     setLoadData(!loadData)   
-    //   },[])
-
-    //   const getUserLogin = async () => {
-    //     const response = await axios.get('https://127.0.0.1:8000/api/auth/user', { headers: {Authorization: 'Bearer ' + jwt}});
-    //     setUserLogin(response.data);
-    //     console.log(response.data)
-    //   }
-
-    //   const loginUser = async (loginUser) => {
-    //     let response= await axios.post('http://127.0.0.1:8000/api/auth/login/', loginUser);
-    //     localStorage.setItem('token', response.data.token);
-    //     console.log("response axios call", response.data.token)
-    //     setJwt(localStorage.getItem('token'));
-    //   }
-
-    //    const logOut = ()=>{
-    //       localStorage.removeItem("token");
-    //       setUser({})
-    //       console.log("User has logged out")
-    //     }
-
+ 
         return (   
             <Router>
               <Header />
