d9e79779eb2c27fe6f90139ea88dc035ce69fa5f
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-12-06 14:31:00 -0500

    Added new component folder and files. Updated react router and react back down to v.16

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,62 @@
 import React, {useState, useEffect} from 'react';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import { HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import { spoonacularAPIKey } from '../keys';
 import './App.css';
+import Login from './Login/Login';
 
 
-function App(){
-    const creator = ['Myles'];
-
-    useEffect(() =>{
-        console.log("The key is", spoonacularAPIKey);
-    })
+function App() {
     
-    return (
-        <h1>App.jsx Component/What's to Eat?</h1>
-    )
+    // const [user, setUser] = useState({})
+    // const [userLogin, setUserLogin] = useState([])
+    // const [jwt, setJwt] = useState()
+    // const [loadData, setLoadData] = useState(false)
+
+    // //Getting token from local storage
+    // const getUserJWT = () => {
+    //     const jwt = localStorage.getItem('token');
+    //     try {
+    //       const user = jwtDecode(jwt);
+    //       setUser(user)
+    //       console.log("get user jwt call", user)
+    //     } catch {
+    //     }
+    //   }
     
+    //   useEffect(() =>{
+    //     getUserJWT();
+    //     getUserLogin();
+    //     setLoadData(!loadData)   
+    //   },[])
+
+    //   const getUserLogin = async () => {
+    //     const response = await axios.get('https://127.0.0.1:8000/api/auth/user', { headers: {Authorization: 'Bearer ' + jwt}});
+    //     setUserLogin(response.data);
+    //     console.log(response.data)
+    //   }
+
+    //   const loginUser = async (loginUser) => {
+    //     let response= await axios.post('http://127.0.0.1:8000/api/auth/login/', loginUser);
+    //     localStorage.setItem('token', response.data.token);
+    //     console.log("response axios call", response.data.token)
+    //     setJwt(localStorage.getItem('token'));
+    //   }
+
+    //    const logOut = ()=>{
+    //       localStorage.removeItem("token");
+    //       setUser({})
+    //       console.log("User has logged out")
+    //     }
+
+        return (   
+            <Router>
+              <Switch>
+                
+              </Switch>
+            </Router>
+        )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import reactDom from 'react-dom';
 import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-reactDom.render(<App />, document.getElementById('root'));
+
+reactDom.render(
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode> 
+    </Router>,
+    document.getElementById('root')
+);
