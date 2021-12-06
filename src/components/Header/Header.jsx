971dbd7d379ec66d04951e568d4f394f9e0a78d8
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-12-06 16:11:55 -0500

    Added a filler NavBar and Landing page for testing purposes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,38 @@ import { Link } from 'react-router-dom';
 
 function Header(){
     return (
-        <div className="row row-space">
-            <div className="col-md-12" style={{padding: 0}}>
-                <div className="titlebar-nav">
-                    <ul className="navbar-nav ml-auto mt-2
-                    mt-lg-0">
-                        <li className="nav-item">
-                            <Link to="/register"
-                            className="nav-link">Register</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/login"
-                            className="nav-link">Login</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Whats To Eat</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost:3000/login">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://localhost:3000/register">Register</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="/profile">My Profile</a></li>
+                  <li><a class="dropdown-item" href="/saved">Saved Recipes</a></li>
+                  <li><a class="dropdown-item" href="/contact">Contact</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+            
     )
 }
 
