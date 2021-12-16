e71815ea9601f99e5fbb5f2387c049e5a6a6f20e
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-12-15 23:27:58 -0500

    Added links to github and linkedin

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import {FaGithub, FaLinkedin} from 'react-icons/fa';
 
-
-function AboutPage(props){
+function AboutPage(){
 
     return (
         <div className="container">
@@ -12,6 +12,8 @@ function AboutPage(props){
                     <p>I have the technological experience in JavaScript, HTML, CSS, React, Python, mySQL, Django Rest Framework, C#, LINQ.</p>
                     <p>I chose to pursue this career field to better myself and my family but also I want to create and innovate for the world. </p>
                     <p>Technology is constantly changing every day and I want to contribute to that change.</p>
+                    <a href="https://github.com/ListerM6"><FaGithub color='4CAF50' size='2rem'/></a>
+                    <a href="https://www.linkedin.com/in/listerm6/"><FaLinkedin color='4CAF50' size='2rem'/></a>
             </ul>
         </div>
     )
